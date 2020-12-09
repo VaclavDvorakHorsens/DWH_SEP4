fe1caf1d74ba1c79f44b8e5cbd45934853bd81ae
cptSmartypants321 <48348762+cptSmartypants321@users.noreply.github.com>
2020-12-09 08:50:22 +0100

    shatf fix?

--- src/main/java/dwh/controllers/EnvironmentDataController.java
+++ src/main/java/dwh/controllers/EnvironmentDataController.java
@@ -57,7 +57,7 @@ public class EnvironmentDataController {
     public /*List<String>*/String getValues() {
 
         EnvironmentalValues test =environmentDataAdapter.getLatestEnvironmentalValue();
-        test.setShaftPos(webSocketConnection.getShaftStatus());
+        test.setShaftPos(shaftAction);
 
         String jsonString = gson.toJson(test);
 
@@ -99,7 +99,7 @@ public class EnvironmentDataController {
         if(action==true) {
             System.out.println("Send value 1");
             environmentDataAdapter.setAction(1);
-            shaftAction=28;
+            shaftAction=1;
             webSocketConnection.sendDownLink(1);
             HttpHeaders headers = new HttpHeaders();
             headers.add("PostAction", "success");
@@ -125,9 +125,9 @@ public class EnvironmentDataController {
         return new ResponseEntity<>(headers, HttpStatus.BAD_REQUEST);
     }
     @GetMapping("/GetAction")
-    public String getAction() {
+    public int getAction() {
 
-        return "" + shaftAction;//+ environmentDataAdapter.getAction();
+        return webSocketConnection.getShaftStatus();//+ environmentDataAdapter.getAction();
 
     }
 }
