859e2f89d4ba77f55f75a9516c1adf3d270d45fb
zounank <283873@via.dk>
2020-12-16 08:36:03 +0100

    Structure changes and Javadoc

--- src/main/java/dwh/controllers/EnvironmentDataController.java
+++ src/main/java/dwh/controllers/EnvironmentDataController.java
@@ -9,17 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 @RestController
 public class EnvironmentDataController {
